4022d3b9a072d2cfc5ffb0ea78463b08185756ca
JJ-vle <jeanjacquesviale.txt@gmail.com>
2026-05-05 21:22:49 +0200

    patchnote bouton valider

--- js/common/infoPage.js
+++ js/common/infoPage.js
@@ -18,6 +18,10 @@ await dataLoaded;
 // Patch notes data
 const patchNotes = [
     {
+        date: "05-05-2026",
+        text: "Fix the validate button (now it should work correctly)."
+    },
+    {
         date: "04-03-2026",
         text: "Small CSS update, mainly focused on the endgame window."
     },
